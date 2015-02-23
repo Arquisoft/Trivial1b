452e57588ae5ea5a23ab7e7fd080bc3eaf4a47e5
MariSanz <marijsanz@gmail.com>
2015-02-23 23:14:42 +0100

    Metodos prueba serializer

--- extract/src/main/java/es/uniovi/asw/trivial/Extractor.java
+++ extract/src/main/java/es/uniovi/asw/trivial/Extractor.java
@@ -22,37 +22,10 @@ public class Extractor {
 	
 	public static void main(String[] args) {
         new Extractor().run(args);
-        List<Question> questions=crearQuestionPrueba();
         JSonSerializer js=new JSonSerializer();
-        String jsString=js.serializer(questions);
-       System.out.println(jsString);
+        
+       
     }	
 	
-	public static List<Question> crearQuestionPrueba(){
-		List<Question> questions=new ArrayList<Question>();
-		List<Answer> answerT=new ArrayList<Answer>();
-		Answer a= new Answer("");
-		Question q= new Question();
-		for(int i=0;i<5;i++){
-			a.setText("Texto respuesta "+i+" true");
-			a.setResponse("Respuesta "+i+" true");
-			
-			answerT.add(a);
-		}
-		List<Answer> answerF=new ArrayList<Answer>();
-		for(int i=0;i<5;i++){
-			a.setText("Texto respuesta "+i+" false");
-			a.setResponse("Respuesta "+i+" false");
-			answerF.add(a);
-		}
-		
-		for(int i=0; i<12; i++){
-			q.setQuestion("Pregunta "+i);
-			q.setTitle("Titulo pregunta "+i);
-			q.setFalseAnswers(answerF);
-			q.setTrueAnswers(answerT);
-			questions.add(q);
-		}
-		return questions;
-	}
+	
 }
