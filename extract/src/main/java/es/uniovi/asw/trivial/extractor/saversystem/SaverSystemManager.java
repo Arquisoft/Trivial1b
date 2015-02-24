67742b7149ffa6a586eb2582a2c1d50d0a3f144f
AdrianBueno <nardian17@gmail.com>
2015-02-24 02:18:32 +0100

    Añado implementaciones de parsers, esqueleto del sistema 2 (SaverSystem) y sistema de loggin a la capa transversal

---
+++ extract/src/main/java/es/uniovi/asw/trivial/extractor/saversystem/SaverSystemManager.java
@@ -0,0 +1,14 @@
+package es.uniovi.asw.trivial.extractor.saversystem;
+
+public class SaverSystemManager {
+	public static void main(String[] args) {
+		new SaverSystemManager();
+	}
+	public SaverSystemManager() {
+		//leer json
+		//parsear json -> modelo
+		//Conectar Base de datos
+		//Actualizar Base de datos.
+	}
+
+}
---
+++ extract/src/main/java/es/uniovi/asw/trivial/extractor/services/parser/imp/XmlParser.java
@@ -0,0 +1,16 @@
+package es.uniovi.asw.trivial.extractor.services.parser.imp;
+
+import java.util.List;
+
+import es.uniovi.asw.trivial.extractor.services.parser.Parser;
+import es.uniovi.asw.trivial.infraestructure.model.Question;
+
+public class XmlParser implements Parser {
+
+	@Override
+	public List<Question> parser(String data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
