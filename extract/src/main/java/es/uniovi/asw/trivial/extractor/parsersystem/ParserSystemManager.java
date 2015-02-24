5de038af4a0aee1f8282fcdcbc1425bedd08ab00
AdrianBueno <nardian17@gmail.com>
2015-02-24 02:30:24 +0100

    Implemento log en la estructura del ParserSystemManager

--- extract/src/main/java/es/uniovi/asw/trivial/extractor/parsersystem/ParserSystemManager.java
+++ extract/src/main/java/es/uniovi/asw/trivial/extractor/parsersystem/ParserSystemManager.java
@@ -6,6 +6,7 @@ import es.uniovi.asw.trivial.extractor.services.parser.Parser;
 import es.uniovi.asw.trivial.extractor.services.serializer.Serializer;
 import es.uniovi.asw.trivial.infraestructure.factories.FactoryService;
 import es.uniovi.asw.trivial.infraestructure.io.Stream;
+import es.uniovi.asw.trivial.infraestructure.log.Log;
 import es.uniovi.asw.trivial.infraestructure.model.Question;
 
 public class ParserSystemManager {
@@ -21,8 +22,13 @@ public class ParserSystemManager {
 		String formatInputFile = "";
 		String formatOutputFile = "";
 		
+		Log.info("Inicio lectura fichero formato: "+formatInputFile);
 		String data = stream.read(pathInputFile);
+		Log.info("Lectura completada!");
+		Log.info("Inicio de procesado de parser");
 		List<Question> questions = parser.parser(data);
+		Log.info("Parseado completado!");//etc
+		Log.info("Inicio serialización a "+formatOutputFile);
 		String jsonData = serializer.serialize(questions);
 		stream.write(pathOutputFile, jsonData);
 		
