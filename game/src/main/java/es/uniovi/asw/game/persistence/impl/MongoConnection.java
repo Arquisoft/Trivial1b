38eb47bdf9d5a8b12ef304206474df6454faa2d5
MariSanz <marijsanz@gmail.com>
2015-04-09 02:55:03 +0200

    Incluyo persistencia MongoDB

---
+++ game/src/main/java/es/uniovi/asw/game/persistence/impl/MongoConnection.java
@@ -0,0 +1,103 @@
+package es.uniovi.asw.trivial.persistence.impl;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.util.JSON;
+
+import es.uniovi.asw.trivial.infraestructure.factories.FactoryService;
+import es.uniovi.asw.trivial.infraestructure.log.impl.Logger;
+
+public class MongoConnection {
+	private static MongoClient conn;
+
+	private static DB db;
+
+	private static DBCollection coll;
+
+	public static final String nombreBD = "trivial1B";
+
+	private static final String fichero = "./preguntas.txt";
+	
+	 
+
+	public static final String nameCollectionQuestion = "QuestionsAnswers";
+	public static final String nameCollectionUser = "Users";
+
+	private static Logger log = FactoryService.getLogService();
+
+	private static MongoConnection instance = null;
+
+	private MongoConnection() {
+		getConnection();
+		conectar();
+		loadBD();
+	}
+
+	public static MongoConnection getInstance() {
+		if (instance == null) {
+			instance = new MongoConnection();
+		}
+		return instance;
+	}
+
+	private MongoClient getConnection() {
+		try {
+			conn = new MongoClient("localhost");
+		} catch (UnknownHostException e) {
+			log.error("Erro al obtener una conexiÃ³n");
+		}
+		return conn;
+	}
+
+	private static DB conectar() {
+		db = conn.getDB(nombreBD);
+		return db;
+	}
+	
+	public  DB getDB(){
+		return db;
+	}
+
+	private static void loadBD() {
+		String s = new String();
+		
+		coll = db.getCollection(nameCollectionQuestion);
+		DBObject doc;
+
+		try {
+			
+			FileReader fr = new FileReader(fichero);
+			BufferedReader br = new BufferedReader(fr);
+
+			while ((s = br.readLine()) != null) {
+				
+				doc = (DBObject) JSON.parse(s);
+				
+				coll.insert(doc);
+			}
+			
+
+				
+
+				
+			
+		} catch (FileNotFoundException e) {
+			log.error("Ruta de fichero no encontrada");
+		} catch (IOException e) {
+			log.error("Error al leer el fichero, fichero vacio");
+		}
+	}
+
+	public DBCollection getCollection() {
+		return coll;
+	}
+
+}
---
+++ game/src/main/java/es/uniovi/asw/game/persistence/impl/PersistenceServiceImpl.java
@@ -0,0 +1,106 @@
+package es.uniovi.asw.trivial.persistence.impl;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+
+import es.uniovi.asw.trivial.infraestructure.model.Answer;
+import es.uniovi.asw.trivial.infraestructure.model.Question;
+import es.uniovi.asw.trivial.persistence.PersistenceService;
+
+public class PersistenceServiceImpl implements PersistenceService {
+
+	private DBCollection coll;
+
+	public PersistenceServiceImpl() {
+
+		coll = MongoConnection.getInstance().getCollection();
+	}
+
+	public List<Question> getQuestionsCategory(String category) {
+		List<Question> list = new ArrayList<Question>();
+		Question q = new Question();
+		Answer a = new Answer();
+		DBObject cursor;
+		Object obj;
+
+		BasicDBObject filtro = new BasicDBObject();
+
+		filtro.put("category", category);
+		DBCursor cur = coll.find(filtro);
+		while (cur.hasNext()) {
+
+			cursor = cur.next();
+			obj = cursor.get("title");
+			q.setTitle(obj.toString());
+
+			obj = cursor.get("text");
+
+			q.setQuestion(obj.toString());
+
+			obj = cursor.get("answersFalse");
+
+			String diaArray[] = obj.toString().split(",");
+			for (String s : diaArray) {
+				a.setResponse(s);
+				a.setCorrect(false);
+				q.addAnswer(a);
+			}
+
+			obj = cursor.get("answersTrue");
+			diaArray = obj.toString().split(",");
+			for (String s : diaArray) {
+				a.setResponse(s);
+				a.setCorrect(true);
+				q.addAnswer(a);
+			}
+
+			list.add(q);
+
+		}
+
+		HashSet<Question> hs = new HashSet<Question>();
+		hs.addAll(list);
+		list.clear();
+		list.addAll(hs);
+
+		return list;
+	}
+
+	/**
+	 * Datos que se meten en la BD del usuario : login, contraseña, si es privilegiado 0 para negar, 1 para acertar,
+	 * nº preguntas acertadas, nº preguntas falladas, nº de partidas ganadas y nº de partidas perdidas
+	 * @param login
+	 * @param passwd
+	 * @param privilegiado
+	 * @param questionSuccess
+	 * @param questionsFailed
+	 * @param matchSuccess
+	 * @param matchFailed
+	 */
+	public void saveUsuario(String login, String passwd, int privilegiado,
+			int questionSuccess, int questionsFailed, int matchSuccess, int matchFailed) {
+		
+		DBCollection coll;
+		coll = (MongoConnection.getInstance().getDB()).getCollection(MongoConnection.nameCollectionUser);
+		
+		DBObject doc = new BasicDBObject();
+		
+		doc.put("login", login);
+		doc.put("passwd", passwd);
+		doc.put("privilegio", privilegiado);
+		doc.put("questionSuccess", questionSuccess);
+		doc.put("questionsFailed", questionsFailed);
+		doc.put("matchSuccess", matchSuccess);
+		doc.put("matchFailed", matchFailed);
+		
+		coll.insert(doc);
+		
+
+	}
+}
