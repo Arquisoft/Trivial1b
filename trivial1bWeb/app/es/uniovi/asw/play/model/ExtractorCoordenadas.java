0bb31aa7ffd771b42c8318290470c42ea7cc0976
Ana Isabel Blanco <anablglez@gmail.com>
2015-04-28 17:51:37 +0200

    Añadido fichero de coordenadas, imagen de tablero, extractor de coordenadas y clases que representan las casillas. JUnit para el extractor de coordenadas. Adapatado html tablero para que cargue la imagen correcta.

---
+++ trivial1bWeb/app/es/uniovi/asw/play/model/ExtractorCoordenadas.java
@@ -0,0 +1,92 @@
+package es.uniovi.asw.play.model;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import es.uniovi.asw.play.model.casillas.Figura;
+import es.uniovi.asw.play.model.casillas.Hexagono;
+import es.uniovi.asw.play.model.casillas.Rectangulo;
+
+/**
+ * Clase que extrae las coordenadas de las casillas de un fichero con formato
+ * numerocasilla:coordenada1/coordenada2/.../coordenadaN
+ * 
+ * @author Ana Isabel Blanco González
+ *
+ */
+public class ExtractorCoordenadas {
+
+	private Map<Integer, Figura> mapa;
+
+	public ExtractorCoordenadas() {
+		leerFichero();
+	}
+
+	private void leerFichero() {
+		File archivo = null;
+		FileReader fr = null;
+		BufferedReader br = null;
+
+		try {
+			// Apertura del fichero y creacion de BufferedReader para poder
+			// hacer una lectura comoda (disponer del metodo readLine()).
+			archivo = new File("app/resources/celdas.txt");
+			fr = new FileReader(archivo);
+			br = new BufferedReader(fr);
+
+			// Lectura del fichero
+			String linea;
+			mapa = new HashMap<Integer, Figura>();
+			while ((linea = br.readLine()) != null) {
+				// Separamos el id de la casilla de las coordenadas.
+				tratarLinea(linea);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// En el finally cerramos el fichero, para asegurarnos
+			// que se cierra tanto si todo va bien como si salta
+			// una excepcion.
+			try {
+				if (null != fr) {
+					fr.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Trocea la línea y carga los puntos.
+	 * 
+	 * @param linea
+	 *            que contiene la casilla y las coordenadas
+	 */
+	private void tratarLinea(String linea) {
+		String[] l = linea.split(":");
+		// Separamos las coordenadas.
+		String[] coord = l[1].split("/");
+		if (coord.length == 4) { // Celda normal
+			Rectangulo r = new Rectangulo(coord);
+			mapa.put(Integer.valueOf(l[0]), r);
+		} else if (coord.length == 6) { // Celda central
+			Hexagono h = new Hexagono(coord);
+			mapa.put(Integer.valueOf(l[0]), h);
+		}
+
+	}
+
+	public Map<Integer, Figura> getMapa() {
+		return mapa;
+	}
+
+	public void setMapa(Map<Integer, Figura> mapa) {
+		this.mapa = mapa;
+	}
+
+}
---
+++ trivial1bWeb/test/ExtractorCoordenadasTest.java
@@ -0,0 +1,44 @@
+
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Point;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.junit.Test;
+
+import es.uniovi.asw.play.model.ExtractorCoordenadas;
+import es.uniovi.asw.play.model.casillas.Figura;
+
+public class ExtractorCoordenadasTest {
+
+	@Test
+	public void test() {
+		ExtractorCoordenadas e = new ExtractorCoordenadas();
+		Point p1 = new Point(355,362);
+		Point p2 = new Point(340,49);
+		Point puntoFuera1 = new Point(342,146);
+		Point puntoFuera2 = new Point(26,22);
+		Map<Integer, Figura> mapa = e.getMapa();
+
+		Iterator<Entry<Integer, Figura>> it = mapa.entrySet().iterator();
+		while (it.hasNext()) {
+			Entry<Integer, Figura> entry = it.next();
+			if (entry.getKey() == 72) {
+				assertTrue(entry.getValue().contains(p1));
+				assertFalse(entry.getValue().contains(p2));
+			} 
+			if(entry.getKey() == 0) {
+				assertTrue(entry.getValue().contains(p2));
+				assertFalse(entry.getValue().contains(p1));
+			}
+			assertFalse(entry.getValue().contains(puntoFuera1));
+			assertFalse(entry.getValue().contains(puntoFuera2));
+		}
+
+	}
+
+}
