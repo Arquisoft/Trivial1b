93f8e215f5aeda4166e063620076630813bf4bd0
María José Sánchez Doria <marijsanz@gmail.com>
2015-05-01 21:24:18 +0200

    Fachada tablero

---
+++ trivial1bWeb/public/javascripts/tableroCoordenada.js
@@ -0,0 +1,49 @@
+var theObj=""; 
+var data;
+
+function toolTip(me) { 
+theObj=me 
+theObj.onmousemove=updatePos; 
+document.getElementById('toolTipBox').innerHTML=window.event.offsetX +" - "+ window.event.offsetY; 
+document.getElementById('toolTipBox').style.display="block"; 
+window.onscroll=updatePos; 
+
+alert(this.data.data);
+
+
+} 
+
+function updatePos() { 
+var ev=arguments[0]?arguments[0]:event; 
+var x=ev.clientX; 
+var y=ev.clientY; 
+diffX=8; 
+diffY=8; 
+document.getElementById('toolTipBox').style.top = y-2+diffY+document.body.scrollTop+ "px"; 
+document.getElementById('toolTipBox').style.left = x-2+diffX+document.body.scrollLeft+"px"; 
+theObj.onmouseout=hideMe; 
+
+// Get a reference to the image you want the pixels of and its dimensions
+var myImage = document.getElementById('tablero');
+var w = myImage.width, h = myImage.height;
+
+// Create a Canvas element
+var canvas = document.createElement('canvas');
+
+// Size the canvas to the element
+canvas.width = w;
+canvas.height = h;
+
+// Draw image onto the canvas
+var ctx = canvas.getContext('2d');
+ctx.drawImage(myImage, 0, 0,w,h);
+
+// Finally, get the image data
+// ('data' is an array of RGBA pixel values for each pixel)
+this.data = ctx.getImageData(y-2+diffY+document.body.scrollTop, x-2+diffX+document.body.scrollLeft, w, h);
+
+
+} 
+function hideMe() { 
+document.getElementById('toolTipBox').style.display="none"; 
+} 
