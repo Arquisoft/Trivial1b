995ce5563c59e4626c4aad35e19c7656808ce755
samuelfdezc <samuelfc93@gmail.com>
2015-05-04 18:31:28 +0200

    Cambios menores ;)

--- trivial1bWeb/app/controllers/Application.java
+++ trivial1bWeb/app/controllers/Application.java
@@ -14,72 +14,65 @@ import play.mvc.Result;
 import views.html.*;
 
 public class Application extends Controller {
-	
+
 	private static Trivial juego = new Trivial();
 	@SuppressWarnings("unused")
-	private static ExtractorCoordenadas coordenadasImagen= new ExtractorCoordenadas(); // ¿?
+	private static ExtractorCoordenadas coordenadasImagen = new ExtractorCoordenadas(); // ¿?
 	static Form<User> userForm = Form.form(User.class);
 	private static UserDb user = new UserDb();
 	static Form<Registro> registerForm = Form.form(Registro.class);
-	
-	 public static Result index() {
-		  
-	    	return ok(login.render(userForm));
-	    }
-	    
-	    public static Result login() {		
-	    	Form<User> filledForm = userForm.bindFromRequest();
-	    	
-			String username = filledForm.field("username").value();
-			String password = filledForm.field("password").value();
-//			System.out.println(username);
-//	    	System.out.println(password);
-			if(user.login(username, password)) {
-				ArrayList<User> usuarios = new ArrayList<User>();
-				usuarios.add(user.lookup(username));
-				juego.setUsuarios(usuarios);
-				return redirect("/tablero");
-				}
-			else {
-				return ok(error.render());
-			}
+
+	public static Result index() {
+
+		return ok(login.render(userForm));
+	}
+
+	public static Result login() {
+		Form<User> filledForm = userForm.bindFromRequest();
+
+		String username = filledForm.field("username").value();
+		String password = filledForm.field("password").value();
+		// System.out.println(username);
+		// System.out.println(password);
+		if (user.login(username, password)) {
+			ArrayList<User> usuarios = new ArrayList<User>();
+			usuarios.add(user.lookup(username));
+			juego.setUsuarios(usuarios);
+			return redirect("/tablero");
+		} else {
+			return ok(error.render());
 		}
-	    
-	    
-    public static Result tablero(){
-    	
-    	return ok(tablero.render());
-    	
-    }
-    
-    public static Result pregunta(String coor){
-    	
-    	String[] coors= coor.split("-");//separo las dos coordenadas para tratarlas como numeros
-    	int coor1= Integer.valueOf(coors[0]);//primea coordenada
-    	int coor2= Integer.valueOf(coors[1]);//segunda coordenada
-    	
-    	juego.setCoordenada1(coor1);
-    	juego.setCoordenada2(coor2);
-    	
-    	
-    	
-    	return redirect("/pregunta");
-    	
-    	
-    	
-    }
-    
-    public static Result logout(){
-    	return ok(logout.render());
-    }
-    
+	}
+
+	public static Result tablero() {
+
+		return ok(tablero.render());
+
+	}
+
+	public static Result pregunta(String coor) {
+
+		String[] coors = coor.split("-");// separo las dos coordenadas para
+											// tratarlas como numeros
+		int coor1 = Integer.valueOf(coors[0]);// primea coordenada
+		int coor2 = Integer.valueOf(coors[1]);// segunda coordenada
+
+		juego.setCoordenada1(coor1);
+		juego.setCoordenada2(coor2);
+
+		return redirect("/pregunta");
+
+	}
+
+	public static Result logout() {
+		return ok(logout.render());
+	}
 
 	public static Result mostrarRegistro() {
 		return ok(registro.render(registerForm));
 	}
-    
-    
-    public static Result registro() {
+
+	public static Result registro() {
 		Form<Registro> filledForm = registerForm.bindFromRequest();
 
 		if (filledForm.hasGlobalErrors())
@@ -88,13 +81,13 @@ public class Application extends Controller {
 
 			String username = filledForm.field("username").value();
 			String password = filledForm.field("password").value();
-			System.out.println(username);
-			System.out.println(password);
+			// System.out.println(username);
+			// System.out.println(password);
 
 			user.addUser(username, password);
 			return ok(login.render(userForm));
 		}
 
 	}
-    
+
 }
--- trivial1bWeb/app/model/Registro.java
+++ trivial1bWeb/app/model/Registro.java
@@ -6,45 +6,46 @@ public class Registro {
 	private String username;
 	private String password;
 	private String password2;
-	
-	public Registro() {}
+
+	public Registro() {
+	}
 
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	
+
 	public String getUsername() {
 		return username;
 	}
-	
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
-	
+
 	public void setPassword2(String password2) {
 		this.password2 = password2;
 	}
-	
+
 	public String getPassword2() {
 		return password2;
 	}
-	
+
 	public String validate() {
 		UserDb user = new UserDb();
-		
-		if(username.equals("") || password.equals("") || password2.equals(""))
+
+		if (username.equals("") || password.equals("") || password2.equals(""))
 			return "Falta algún dato por introducir";
-			
-		else if(user.lookup(username) != null)
+
+		else if (user.lookup(username) != null)
 			return "Ya existe un usuario con este identificicador";
-		
-		else if(!password.equals(password2))
+
+		else if (!password.equals(password2))
 			return "Las contraseñas no coinciden";
-				
+
 		return null;
 	}
 }
